43eefd9527a672d2f009183f75943877fb008efc
Daxiong <106467653+JunxiongKe@users.noreply.github.com>
2023-06-15 19:25:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,78 +7,68 @@ const s3 = new AWS.S3();
 // Lambda 处理程序
 exports.handler = async (event) => {
   let compressedImageBuffer;
-  console.log(".........compress start");  
+  console.log("......... start");
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(event.Records[0].s3.object.key);
   console.log("the image is :" + key);
-
 
   try {
     // 下载原始图像文件
     const originalImage = await s3.getObject({ Bucket: bucket, Key: key }).promise();
 
     // 检测图像格式
-    const imageMetadata = await sharp(originalImage.Body, { animated: true ,limitInputPixels: false}).metadata();
-
-
-    //const format = await sharp(originalImage.Body,{ animated: true ,limitInputPixels: false}).metadata().then((metadata) => metadata.format);
-
-    const originalWidth = imageMetadata.width;
-    const originalHeight = imageMetadata.pageHeight;
-    const format = imageMetadata.format;
+    const imageMetadata = await sharp(originalImage.Body, { animated: true, limitInputPixels: false }).metadata();
+    const format = imageMetadata.format.toLowerCase();
 
     // 限制压缩的图片格式范围
-    const allowedFormats = ['png', 'gif', 'jpeg', 'webp'];
+    const allowedFormats = ['png', 'avif', 'jpeg', 'webp', 'jpg'];
 
     if (!allowedFormats.includes(format)) {
       console.log(`Unsupported image format: ${format}`);
+      // 对于不支持的格式，直接将原始图片复制到目标目录
+      const targetKey = key.replace('source-images/', 'target-images/');
+      await s3
+        .copyObject({
+          Bucket: bucket,
+          CopySource: `${bucket}/${key}`,
+          Key: targetKey,
+        })
+        .promise();
+
+      console.log(".........put file to s3 end");
+      console.log("......... end");
+
       return {
-        statusCode: 400,
-        body: 'Unsupported image format',
+        statusCode: 200,
+        body: 'Image c successful',
       };
     }
 
-    // 设置默认压缩选项
-    let compressionOptions = {
-      lossless: true,
-    };
-
     // 根据图像格式调整压缩选项
+    let compressionOptions = {};
     if (format === 'jpeg' || format === 'jpg') {
       compressionOptions = { quality: 70 };
     } else if (format === 'png') {
-      compressionOptions = { compressionLevel: 7, lossless: true };
+      compressionOptions = { quality: 70, lossless: true };
     } else if (format === 'webp') {
-      compressionOptions = { quality: 70,lossless: true ,animated: true};
-    } else if (format === 'gif') {
-      compressionOptions = { lossless: true, colors: 256, animated: true };
+      compressionOptions = { quality: 70, animated: true };
+    } else if (format === 'avif') {
+      compressionOptions = { lossless: true, quality: 70 };
     }
-    
-    if (format === 'gif') {
-      console.log(".........gif format compress start");  
-      // 获取GIF的元数据
 
-      const targetWidth = Math.floor(originalWidth * 0.3);
-      const targetHeight = Math.floor(originalHeight * 0.3);
+    console.log(".........compress start");
+    // 使用 Sharp 进行无损压缩
+    compressedImageBuffer = await sharp(originalImage.Body, { limitInputPixels: false })
+      .toFormat(format, compressionOptions)
+      .toBuffer();
+    console.log(".........compress end");
 
-      // 使用Sharp库调整GIF的尺寸并压缩
-      compressedImageBuffer = await sharp(originalImage.Body, { animated: true ,limitInputPixels: false ,effort : 1})
-        .resize(targetWidth, targetHeight)
-        .toBuffer();
-      console.log(".........gif format compress end");  
-    } else {
-      console.log(".........other format compress start");  
-      // 使用 Sharp 进行无损压缩
-      compressedImageBuffer = await sharp(originalImage.Body,{ limitInputPixels: false, effort : 1})
-        .toFormat(format, compressionOptions)
-        .toBuffer();
-      console.log(".........other format compress end");    
-    }
+    console.log(".........put file to s3 start");
 
     // 构建目标文件的 Key（路径）
     const targetKey = key.replace('source-images/', 'target-images/');
 
-    // 将处理后的GIF图像上传到同一个存储桶的目标文件夹
+    // 将处理后的图片上传到同一个存储桶的目标文件夹
     await s3
       .putObject({
         Body: compressedImageBuffer,
@@ -87,7 +77,8 @@ exports.handler = async (event) => {
       })
       .promise();
 
-    console.log(".........compress end");
+    console.log(".........put file to s3 end");
+    console.log("......... end");
 
     return {
       statusCode: 200,
